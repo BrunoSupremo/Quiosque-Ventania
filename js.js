f2b151c4f85c38d680917c85da3e5261441a4177
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2021-05-02 23:50:43 -0300

    fade in pra esconder imagem carregando

--- js.js
+++ js.js
@@ -2,7 +2,18 @@ function toggle_visibility(){
 	this.classList.toggle("visible");
 }
 
-var items_cardapio = document.querySelectorAll('#cardapio div');
-for (var i = 0; i < items_cardapio.length; i++) {
+let items_cardapio = document.querySelectorAll('#cardapio div');
+for (let i = 0; i < items_cardapio.length; i++) {
 	items_cardapio[i].onclick = toggle_visibility;
+}
+
+let imagens_cardapio = document.querySelectorAll('#cardapio div img');
+for (let i = 0; i < imagens_cardapio.length; i++) {
+	if (imagens_cardapio[i].complete){
+		imagens_cardapio[i].classList.toggle("visible");
+	}else{
+		imagens_cardapio[i].onload = function(){
+			this.classList.toggle("visible");
+		};
+	}
 }
